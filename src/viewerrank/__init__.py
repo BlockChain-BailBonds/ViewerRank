eccbbc83f65195cf71875e04abcfe97dec741c0f
Nine1Eight <founder918tech@gmail.com>
2026-08-10 13:43:04 -0500

    Export ViewerRank scoring API

---
+++ src/viewerrank/__init__.py
@@ -0,0 +1,23 @@
+from .scoring import (
+    DEFAULT_WEIGHTS,
+    ViewerComponents,
+    bayesian_shrink,
+    calculate_viewer_rank,
+    confidence_from_samples,
+    discovery_credit,
+    early_factor,
+    selection_alpha,
+    time_decay,
+)
+
+__all__ = [
+    "DEFAULT_WEIGHTS",
+    "ViewerComponents",
+    "bayesian_shrink",
+    "calculate_viewer_rank",
+    "confidence_from_samples",
+    "discovery_credit",
+    "early_factor",
+    "selection_alpha",
+    "time_decay",
+]
